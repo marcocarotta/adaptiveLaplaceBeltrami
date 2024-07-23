8285d8ed4c534557b04f2f45b35e291b95be0896
Marco Carotta <marcocarotta2000@gmail.com>
2024-07-23 21:34:46 +0200

    minor changes to run.sh. added .vtk from the 2d-£d program in folder output

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker rm lbp
 docker run --name lbp coupled_lb_p
 
 mkdir -p output
-docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/laplacebeltrami_grid-0.gnuplot ./output/
-docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/laplacebeltrami_solution2d-cycle_0.vtk ./output/
-docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/poisson_grid-0.gnuplot ./output/
-docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/poisson_solution2d-cycle_0.vtk ./output/
+docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/laplacebeltrami_grid-*.gnuplot ./output/
+docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/laplacebeltrami_solution*d-cycle_*.vtk ./output/
+docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/poisson_grid-*.gnuplot ./output/
+docker cp lbp:/usr/src/coupled-LaplaceBeltrami-Poisson/poisson_solution*d-cycle_*.vtk ./output/
